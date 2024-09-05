06d7408aa58bbfa945680828499fdac1098e58bd
Carlos Pereira de Castro Filho <50716871+carloscastrofilho@users.noreply.github.com>
2024-09-05 12:57:56 -0300

    projeto modelo basico

---
+++ barraAcessibilidade.js
@@ -0,0 +1,17 @@
+
+
+const widget = new window.VLibras.Widget('https://vlibras.gov.br/app');
+const toggleButton = document.getElementById('toggleVlibras');
+let vlibrasActive = true;
+
+toggleButton.addEventListener('click', () => {
+    const vlibrasElement = document.querySelector('[vw]');
+    if (vlibrasActive) {
+        vlibrasElement.style.display = 'none';
+        toggleButton.textContent = 'Ativar VLibras';
+    } else {
+        vlibrasElement.style.display = 'block';
+        toggleButton.textContent = 'Desativar VLibras';
+    }
+    vlibrasActive = !vlibrasActive;
+});
